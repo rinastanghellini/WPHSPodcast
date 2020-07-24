9433459cdd78bb7d4692dabf9d409a30e73621fa
Rina Stanghellini <rfzs1031@gmail.com>
2020-07-24 14:10:59 -0400

    tried to decrease

--- app.js
+++ app.js
@@ -24,17 +24,17 @@ db.once('open', function() {
   console.log("we are connected!!!")
 });
 
-const commentController = require('./controllers/commentController')
-const profileController = require('./controllers/profileController')
-const forumPostController = require('./controllers/forumPostController')
+// const commentController = require('./controllers/commentController')
+// const profileController = require('./controllers/profileController')
+// const forumPostController = require('./controllers/forumPostController')
 
 // Authentication
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-// here we set up authentication with passport
-const passport = require('passport')
-const configPassport = require('./config/passport')
-configPassport(passport)
-
+// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// // here we set up authentication with passport
+// const passport = require('passport')
+// const configPassport = require('./config/passport')
+// configPassport(passport)
+//
 
 var app = express();
 
@@ -57,55 +57,55 @@ app.use(express.static(path.join(__dirname, 'public')));
      HERE ARE THE AUTHENTICATION ROUTES
 **************************************************************************/
 
-app.use(session(
-  { secret: 'zzbbyanana',
-    resave: false,
-    saveUninitialized: false }));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(session(
+//   { secret: 'zzbbyanana',
+//     resave: false,
+//     saveUninitialized: false }));
+// app.use(flash());
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(bodyParser.urlencoded({ extended: false }));
+//
+//
 
-
-
-const approvedLogins = ["tjhickey724@gmail.com","csjbs2018@gmail.com"];
+// const approvedLogins = ["tjhickey724@gmail.com","csjbs2018@gmail.com"];
 
 // here is where we check on their logged in status
-app.use((req,res,next) => {
-  res.locals.title="YellowCartwheel"
-  res.locals.loggedIn = false
-  if (req.isAuthenticated()){
-      console.log("user has been Authenticated")
-      res.locals.user = req.user
-      res.locals.loggedIn = true
-    }
-  else {
-    res.locals.loggedIn = false
-  }
-  next()
-})
+// app.use((req,res,next) => {
+//   res.locals.title="YellowCartwheel"
+//   res.locals.loggedIn = false
+//   if (req.isAuthenticated()){
+//       console.log("user has been Authenticated")
+//       res.locals.user = req.user
+//       res.locals.loggedIn = true
+//     }
+//   else {
+//     res.locals.loggedIn = false
+//   }
+//   next()
+// })
 
 
-
-// here are the authentication routes
-
-app.get('/loginerror', function(req,res){
-  res.render('loginerror',{})
-})
-
-app.get('/login', function(req,res){
-  res.render('login',{})
-})
-
-
-
-// route for logging out
-app.get('/logout', function(req, res) {
-        req.session.destroy((error)=>{console.log("Error in destroying session: "+error)});
-        console.log("session has been destroyed")
-        req.logout();
-        res.redirect('/');
-    });
+//
+// // here are the authentication routes
+//
+// app.get('/loginerror', function(req,res){
+//   res.render('loginerror',{})
+// })
+//
+// app.get('/login', function(req,res){
+//   res.render('login',{})
+// })
+//
+//
+//
+// // route for logging out
+// app.get('/logout', function(req, res) {
+//         req.session.destroy((error)=>{console.log("Error in destroying session: "+error)});
+//         console.log("session has been destroyed")
+//         req.logout();
+//         res.redirect('/');
+//     });
 
 
 // =====================================
@@ -114,48 +114,48 @@ app.get('/logout', function(req, res) {
 // send to google to do the authentication
 // profile gets us their basic information including their name
 // email gets their emails
-app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
-
-
-app.get('/login/authorized',
-        passport.authenticate('google', {
-                successRedirect : '/',
-                failureRedirect : '/loginerror'
-        })
-      );
-
-
-// route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-    console.log("checking to see if they are authenticated!")
-    // if user is authenticated in the session, carry on
-    res.locals.loggedIn = false
-    if (req.isAuthenticated()){
-      console.log("user has been Authenticated")
-      res.locals.loggedIn = true
-      return next();
-    } else {
-      console.log("user has not been authenticated...")
-      res.redirect('/login');
-    }
-}
+// app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+//
+//
+// app.get('/login/authorized',
+//         passport.authenticate('google', {
+//                 successRedirect : '/',
+//                 failureRedirect : '/loginerror'
+//         })
+//       );
+//
+//
+// // route middleware to make sure a user is logged in
+// function isLoggedIn(req, res, next) {
+//     console.log("checking to see if they are authenticated!")
+//     // if user is authenticated in the session, carry on
+//     res.locals.loggedIn = false
+//     if (req.isAuthenticated()){
+//       console.log("user has been Authenticated")
+//       res.locals.loggedIn = true
+//       return next();
+//     } else {
+//       console.log("user has not been authenticated...")
+//       res.redirect('/login');
+//     }
+// }
 
 // we require them to be logged in to see their profile
-app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile')
-    });
-
-app.get('/editProfile',isLoggedIn, (req,res)=>{
-  res.render('editProfile')
-})
-
-
-
-app.get('/profiles', isLoggedIn, profileController.getAllProfiles);
-app.get('/showProfile/:id', isLoggedIn, profileController.getOneProfile);
-
-
-app.post('/updateProfile',profileController.update)
+// app.get('/profile', isLoggedIn, function(req, res) {
+//         res.render('profile')
+//     });
+//
+// app.get('/editProfile',isLoggedIn, (req,res)=>{
+//   res.render('editProfile')
+// })
+//
+//
+//
+// app.get('/profiles', isLoggedIn, profileController.getAllProfiles);
+// app.get('/showProfile/:id', isLoggedIn, profileController.getOneProfile);
+//
+//
+// app.post('/updateProfile',profileController.update)
 
 // add page for editProfile and views
 // add router for updateProfile and send browser to /profie
@@ -227,39 +227,39 @@ app.get('/Podcasts', (req, res) => {
   res.render('Podcasts',{title:"Podcasts"});
 });
 
-app.get('/forum',forumPostController.getAllForumPosts)
+// app.get('/forum',forumPostController.getAllForumPosts)
+//
+// app.post('/forum',forumPostController.saveForumPost)
+//
+// app.post('/forumDelete',forumPostController.deleteForumPost)
+//
+// app.get('/showPost/:id',
+//         forumPostController.attachAllForumComments,
+//         forumPostController.showOnePost)
+//
+// app.get('/showPostComments/:id',
+//         forumPostController.attachAllForumComments,
+//         (req,res)=>{
+//           res.render('forumPostComments',{title:"comments"})
+//         })
+//
+// app.post('/saveForumComment',forumPostController.saveForumComment)
+//
 
-app.post('/forum',forumPostController.saveForumPost)
+// // myform demo ...
+//
+// app.post('/processform', commentController.saveComment)
+//
+// app.get('/showComments', commentController.getAllComments)
+// // app.use('/', indexRouter);  // this is how we use a router to handle the / path
+// // but here we are more direct
 
-app.post('/forumDelete',forumPostController.deleteForumPost)
-
-app.get('/showPost/:id',
-        forumPostController.attachAllForumComments,
-        forumPostController.showOnePost)
-
-app.get('/showPostComments/:id',
-        forumPostController.attachAllForumComments,
-        (req,res)=>{
-          res.render('forumPostComments',{title:"comments"})
-        })
-
-app.post('/saveForumComment',forumPostController.saveForumComment)
-
-
-// myform demo ...
-
-app.post('/processform', commentController.saveComment)
-
-app.get('/showComments', commentController.getAllComments)
-// app.use('/', indexRouter);  // this is how we use a router to handle the / path
-// but here we are more direct
-
-app.get('/showComment/:id', commentController.getOneComment)
-
-function processFormData(req,res,next){
-  res.render('formdata',
-     {title:"Form Data",url:req.body.url, coms:req.body.theComments})
-}
+// app.get('/showComment/:id', commentController.getOneComment)
+//
+// function processFormData(req,res,next){
+//   res.render('formdata',
+//      {title:"Form Data",url:req.body.url, coms:req.body.theComments})
+// }
 
 
 
